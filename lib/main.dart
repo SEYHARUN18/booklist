de35a760b0e186cdc697ed00db097bc6c2aa4d17
RON SEYHA <ronseyha8@gmail.com>
2024-09-13 18:21:15 +0700

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: deprecated_member_use
 
-import 'dart:developer';
-
 import 'package:book_list/config/app_config.dart';
 import 'package:book_list/environment/environment.dart';
 import 'package:book_list/router/app_router.gr.dart';
@@ -14,41 +12,24 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() async {
-  BlocOverrides.runZoned(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    GetIt.instance.registerSingleton<AppRouter>(AppRouter());
-    final storage = await HydratedStorage.build(
-      storageDirectory: await getApplicationDocumentsDirectory(),
-    );
-    HydratedBlocOverrides.runZoned(
-      () {
-        AppConfig(env: Environment.dev());
-
-        return runApp(
-          ScreenUtilInit(
-            designSize: const Size(390, 844),
-            minTextAdapt: true,
-            builder: (context, child) => const Initialize(),
-          ),
-        );
-      },
-      storage: storage,
-    );
-  }, blocObserver: AppBlocObserver());
-}
-
-class AppBlocObserver extends BlocObserver {
-  @override
-  void onChange(BlocBase bloc, Change change) {
-    super.onChange(bloc, change);
-    if (bloc is Cubit) log('$change');
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
-    log('$transition');
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  AppConfig(env: Environment.dev());
+  GetIt.instance.registerSingleton<AppRouter>(AppRouter());
+  final storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+  HydratedBlocOverrides.runZoned(
+    () {
+      return runApp(
+        ScreenUtilInit(
+          designSize: const Size(390, 844),
+          minTextAdapt: true,
+          builder: (context, child) => const Initialize(),
+        ),
+      );
+    },
+    storage: storage,
+  );
 }
 
 class Initialize extends HookWidget {
